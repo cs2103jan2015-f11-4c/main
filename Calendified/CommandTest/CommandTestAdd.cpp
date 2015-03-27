5870e3a825c07665e50efd11f2b36d78c786e7e4
jingzhen <cheejingzhen@hotmail.com>
2015-03-27 10:10:43 +0800

    Unit testing for taskAdd

--- Calendified/CommandTest/CommandTestAdd.cpp
+++ Calendified/CommandTest/CommandTestAdd.cpp
@@ -1,3 +1,4 @@
+//@author A0116027R
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
@@ -8,10 +9,28 @@ namespace CommandTestAdd
 	TEST_CLASS(CommandTestAdd)
 	{
 	public:
-		
-		TEST_METHOD(CommandTestAdd_)
+		taskAdd testTaskAdd;
+
+		TEST_METHOD(CommandTestAdd_taskAddTask)
 		{
-			// TODO: Your test code here
+			std::string expectedResult = "Added succesfully!";
+			std::string fail = "Task not added. Please try again.";
+			std::string testTaskTypeM = "main";
+			std::string testTaskTypeF = "float"; 
+			std::string testStringM = "13/03/2015 13:00 visit school @utown";
+			std::string testStringF = "visit school";
+
+			testTaskAdd.setTask(testStringM);
+			testTaskAdd.setTaskType(testTaskTypeM);
+			std::string actualResult1 = testTaskAdd.taskAddTask();
+			Assert::AreEqual(fail, actualResult1); //storage cannnot find filepath
+			//test case 1
+			testTaskAdd.setTask(testStringF);
+			testTaskAdd.setTaskType(testTaskTypeF);
+			std::string actualResult2 = testTaskAdd.taskAddTask();
+			Assert::AreEqual(fail, actualResult2); //storage cannot find filepath
+			//test case 2
+
 		}
 
 	};
